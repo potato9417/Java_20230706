cba73896ce173935d8201088cce146793656f843
potato9417 <59465217+potato9417@users.noreply.github.com>
2023-07-06 17:11:57 +0900

    설치 및 helloWorld

---
+++ java/jsd_20230706_01/src/sample/JavaEx01.java
@@ -0,0 +1,12 @@
+package sample;
+
+public class JavaEx01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// sysout + Ctrl + space => 자동완성
+		System.out.println("Hello World!");
+		
+	}
+
+}
