29b3a4b3703e10595a62fe5fac8e3cff6eef9090
potato9417 <59465217+potato9417@users.noreply.github.com>
2023-07-26 14:23:45 +0900

    0726_오류 해결 (.trim사용)
    단어별 갯수확인시 앞의 공백도 갯수로 포함되는 부분을 .trim()을 사용하여 앞뒤공백을 제거한 후 진행

--- jse_text/src/com/test0725/jse/JavaTest02.java
+++ jse_text/src/com/test0725/jse/JavaTest02.java
@@ -11,16 +11,13 @@ public class JavaTest02 {
 //		System.out.print("문자를 입력하세요 : ");
 //		String str = sc.nextLine();
 		
-		String str = "The first character is a blank";
+		String str = "   The first character is a blank";
 //		System.out.println(str);
 
 		
-		String strArr[] = str.split(" ");
-		for(int i=0; i<strArr.length;i++) {
-			if((int)strArr[i]==0) {
-				
-			}
-		}
+		// .trim() : 앞뒤 공백제거
+		String strArr[] = str.trim().split(" ");
+
 		System.out.print(strArr.length);
 		
 		
